8ba5d1b8036daa9ebb8e24684c9cdaa397a03041
Gendy B <w0202057@campus.nscc.ca>
2024-10-10 14:00:34 -0300

    updated the code

--- about-page.php
+++ about-page.php
@@ -3,82 +3,85 @@
  * Template Name: About Page
  */
 
-get_header(); // Include the heade
-
+get_header(); // Include the header
 ?>
 
 <main class="main">
     <!--===== About =====-->
     <section class="about section" id="about">
-        <h2 data-heading="My Intro" class="section_title">About Me</h2>
+        <h2 data-heading="My Intro" class="section_title"><?php esc_html_e('About Me', 'portfolio');?></h2>
         <div class="about_container container grid">
-            <img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/blackman.jpg" alt="" class="about_img">
+            <img src="<?php echo esc_url(get_template_directory_uri() . '/img/blackman.jpg'); ?>"
+                alt="<?php esc_attr_e('Image of Gendy', 'portfolio');?>" class="about_img">
             <div class="about_data">
-                <h3 class="about_heading">Hi, I am Gendy, based in Canada</h3>
-                <p class="about_description">I am in my second year at NSCC in the IT Web Programming program. I am a
-                    Web Developer,
-                    Programmer, and Entrepreneur. That being said, I am also a person with a disability, but that has
-                    not stopped me
-                    from achieving my goals.</p>
+                <h3 class="about_heading"><?php esc_html_e('Hi, I am Gendy, based in Canada', 'portfolio');?></h3>
+                <p class="about_description">
+                    <?php esc_html_e('I am in my second year at NSCC in the IT Web Programming program. I am a Web Developer, Programmer, and Entrepreneur. That being said, I am also a person with a disability, but that has not stopped me from achieving my goals.', 'portfolio');?>
+                </p>
                 <div class="about_info grid">
                     <div class="about_box">
                         <i class="uil uil-award about_icon"></i>
-                        <h3 class="about_title">Experience</h3>
+                        <h3 class="about_title"><?php esc_html_e('Experience', 'portfolio');?></h3>
                         <span class="about_subtitle">2 Years</span>
                     </div>
                     <div class="about_box">
                         <i class="uil uil-suitcase-alt about_icon"></i>
-                        <h3 class="about_title">Completed</h3>
+                        <h3 class="about_title"><?php esc_html_e('Completed', 'portfolio');?></h3>
                         <span class="about_subtitle">5 Projects</span>
                     </div>
                     <div class="about_box">
                         <i class="uil uil-headphones-alt about_icon"></i>
-                        <h3 class="about_title">Support</h3>
-                        <span class="about_subtitle">Online 24/7</span>
+                        <h3 class="about_title"><?php esc_html_e('Support', 'portfolio');?></h3>
+                        <span class="about_subtitle"><?php esc_html_e('Online 24/7', 'portfolio');?></span>
                     </div>
                 </div>
                 <a href="<?php echo esc_url(home_url('/contact')); ?>" class="button">
                     <i class="uil uil-navigator button-icon"></i>
-                    Contact Me
+                    <?php esc_html_e('Contact Me', 'portfolio');?>
                 </a>
             </div>
         </div>
     </section>
+
     <!--===== Qualification =====-->
     <section class="qualification sections">
-        <h2 data-heading=" My Journey" class="section_title">Qualification</h2>
+        <h2 data-heading="My Journey" class="section_title"><?php esc_html_e('Qualification', 'portfolio');?></h2>
         <div class="qualification_container container grid">
             <div class="education">
                 <h3 class="qualification_title">
                     <i class="uil uil-graduation-cap"></i>
-                    Education
+                    <?php esc_html_e('Education', 'portfolio');?>
                 </h3>
                 <div class="timeline">
                     <div class="timeline_item">
                         <div class="circle_dot"></div>
-                        <h3 class="timeline_title">NSCC - Institute of Technology Campus - Halifax, NS</h3>
-                        <p class="timeline_text">IT Web Programming</p>
+                        <h3 class="timeline_title">
+                            <?php esc_html_e('NSCC - Institute of Technology Campus - Halifax, NS', 'portfolio');?></h3>
+                        <p class="timeline_text"><?php esc_html_e('IT Web Programming', 'portfolio');?></p>
                         <span class="timeline_date">
                             <i class="uil uil-calendar-alt"></i>
-                            2021 - Present
+                            <?php esc_html_e('2021 - Present', 'portfolio');?>
                         </span>
                     </div>
                     <div class="timeline_item">
                         <div class="circle_dot"></div>
-                        <h3 class="timeline_title">Mount Saint Vincent University - Halifax, NS</h3>
-                        <p class="timeline_text">Bachelor of Hospitality and Tourism Management</p>
+                        <h3 class="timeline_title">
+                            <?php esc_html_e('Mount Saint Vincent University - Halifax, NS', 'portfolio');?></h3>
+                        <p class="timeline_text">
+                            <?php esc_html_e('Bachelor of Hospitality and Tourism Management', 'portfolio');?></p>
                         <span class="timeline_date">
                             <i class="uil uil-calendar-alt"></i>
-                            2016 - 2020
+                            <?php esc_html_e('2016 - 2020', 'portfolio');?>
                         </span>
                     </div>
                     <div class="timeline_item">
                         <div class="circle_dot"></div>
-                        <h3 class="timeline_title">Algonquin College - Ottawa, Ont</h3>
-                        <p class="timeline_text">Diploma in Culinary Management</p>
+                        <h3 class="timeline_title"><?php esc_html_e('Algonquin College - Ottawa, Ont', 'portfolio');?>
+                        </h3>
+                        <p class="timeline_text"><?php esc_html_e('Diploma in Culinary Management', 'portfolio');?></p>
                         <span class="timeline_date">
                             <i class="uil uil-calendar-alt"></i>
-                            2006 - 2007
+                            <?php esc_html_e('2006 - 2007', 'portfolio');?>
                         </span>
                     </div>
                 </div>
@@ -86,34 +89,38 @@ get_header(); // Include the heade
             <div class="experience">
                 <h3 class="qualification_title">
                     <i class="uil uil-suitcase"></i>
-                    Experience
+                    <?php esc_html_e('Experience', 'portfolio');?>
                 </h3>
                 <div class="timeline">
                     <div class="timeline_item">
                         <div class="circle_dot"></div>
-                        <h3 class="timeline_title">Maritimes Lens & Equipment Rentals, Halifax, Nova Scotia</h3>
-                        <p class="timeline_text">Founder</p>
+                        <h3 class="timeline_title">
+                            <?php esc_html_e('Maritimes Lens & Equipment Rentals, Halifax, Nova Scotia', 'portfolio');?>
+                        </h3>
+                        <p class="timeline_text"><?php esc_html_e('Founder', 'portfolio');?></p>
                         <span class="timeline_date">
                             <i class="uil uil-calendar-alt"></i>
-                            Aug 2020 - Present
+                            <?php esc_html_e('Aug 2020 - Present', 'portfolio');?>
                         </span>
                     </div>
                     <div class="timeline_item">
                         <div class="circle_dot"></div>
-                        <h3 class="timeline_title">Nova Scotia Health, Halifax, Nova Scotia</h3>
-                        <p class="timeline_text">Central Laundry</p>
+                        <h3 class="timeline_title">
+                            <?php esc_html_e('Nova Scotia Health, Halifax, Nova Scotia', 'portfolio');?></h3>
+                        <p class="timeline_text"><?php esc_html_e('Central Laundry', 'portfolio');?></p>
                         <span class="timeline_date">
                             <i class="uil uil-calendar-alt"></i>
-                            March 2011 - Present
+                            <?php esc_html_e('March 2011 - Present', 'portfolio');?>
                         </span>
                     </div>
                     <div class="timeline_item">
                         <div class="circle_dot"></div>
-                        <h3 class="timeline_title">Nova Scotia Health, Halifax, Nova Scotia</h3>
-                        <p class="timeline_text">Food & Nutrition Services</p>
+                        <h3 class="timeline_title">
+                            <?php esc_html_e('Nova Scotia Health, Halifax, Nova Scotia', 'portfolio');?></h3>
+                        <p class="timeline_text"><?php esc_html_e('Food & Nutrition Services', 'portfolio');?></p>
                         <span class="timeline_date">
                             <i class="uil uil-calendar-alt"></i>
-                            March 2020 - March 2021
+                            <?php esc_html_e('March 2020 - March 2021', 'portfolio');?>
                         </span>
                     </div>
                 </div>
--- home-page.php
+++ home-page.php
@@ -4,7 +4,6 @@
  */
 
 get_header(); // Include the header
-
 ?>
 
 <main class="main">
@@ -12,49 +11,51 @@ get_header(); // Include the header
     <section class="home" id="home">
         <div class="home_container container grid">
             <div class="home_social">
-                <span class="home_social-follow">Follow Me</span>
+                <span class="home_social-follow"><?php esc_html_e('Follow Me', 'portfolio');?></span>
                 <div class="home_social-links">
-                    <a href="https://www.facebook.com" target="_blank" class="home_social-link">
+                    <a href="https://www.facebook.com" target="_blank" class="home_social-link"
+                        rel="noopener noreferrer">
                         <i class="uil uil-facebook-f"></i>
                     </a>
-                    <a href="https://www.instagram.com" target="_blank" class="home_social-link">
+                    <a href="https://www.instagram.com" target="_blank" class="home_social-link"
+                        rel="noopener noreferrer">
                         <i class="uil uil-instagram"></i>
                     </a>
-                    <a href="https://twitter.com" target="_blank" class="home_social-link">
+                    <a href="https://twitter.com" target="_blank" class="home_social-link" rel="noopener noreferrer">
                         <i class="uil uil-twitter"></i>
                     </a>
                 </div>
             </div>
-            <img src="<?php echo esc_url(get_template_directory_uri()); ?>assets/img/blackman.jpg" alt=""
-                class="home_img">
+            <img src="<?php echo esc_url(get_template_directory_uri() . '/assets/img/blackman.jpg'); ?>"
+                alt="<?php esc_attr_e('Gendy Image', 'portfolio');?>" class="home_img">
             <div class="home_data">
-                <h1 class="home_titl">Hello, my name is Gendy</h1>
-                <h3 class="home_subtitle">I'm <span class="typing"></span>.</h3>
-                <!-- <p class="home_description">I am in my second year at NSCC in the IT Web Programming, program.</p> -->
+                <h1 class="home_titl"><?php esc_html_e('Hello, my name is Gendy', 'portfolio');?></h1>
+                <h3 class="home_subtitle"><?php esc_html_e("I'm ", 'portfolio');?><span class="typing"></span>.</h3>
+                <!-- <p class="home_description"><?php esc_html_e('I am in my second year at NSCC in the IT Web Programming program.', 'portfolio');?></p> -->
                 <a href="<?php echo esc_url(home_url('/about')); ?>" class="button">
                     <i class="uil uil-user button_icon"></i>
-                    More About Me
+                    <?php esc_html_e('More About Me', 'portfolio');?>
                 </a>
             </div>
             <div class="my_info">
                 <div class="info_item">
                     <i class="uil uil-facebook-messenger info_icon"></i>
                     <div>
-                        <h3 class="info_title">Messenger</h3>
+                        <h3 class="info_title"><?php esc_html_e('Messenger', 'portfolio');?></h3>
                         <span class="info_subtitle">user.fb123</span>
                     </div>
                 </div>
                 <div class="info_item">
                     <i class="uil uil-whatsapp info_icon"></i>
                     <div>
-                        <h3 class="info_title">WhatsApp</h3>
+                        <h3 class="info_title"><?php esc_html_e('WhatsApp', 'portfolio');?></h3>
                         <span class="info_subtitle">999-888-777</span>
                     </div>
                 </div>
                 <div class="info_item">
                     <i class="uil uil-envelope-edit info_icon"></i>
                     <div>
-                        <h3 class="info_title">Email</h3>
+                        <h3 class="info_title"><?php esc_html_e('Email', 'portfolio');?></h3>
                         <span class="info_subtitle">user@mail.com</span>
                     </div>
                 </div>
@@ -62,3 +63,5 @@ get_header(); // Include the header
         </div>
     </section>
 </main>
+
+<?php get_footer(); // Include the footer ?>
