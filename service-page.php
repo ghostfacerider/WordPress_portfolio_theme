be9558a21061219a5dc4e5ab6362e7d474bc48cf
sonukumaard <sinukumar.du@gmail.com>
2024-10-21 10:41:52 +0530

    Update service-page.php

--- service-page.php
+++ service-page.php
@@ -1,47 +1,131 @@
 <?php
 /**
- * Template Name: Service Page
+ * Template Name: Services
  */
 
-get_header(); // Include the heade
+get_header(); // Include the header
 ?>
 <main class="main">
-    <!--========== Services  ==========-->
-    <section class="services section" id="services">
-        <h2 data-heading="Services" class="section_title"><?php the_field('services_title'); ?></h2>
-        <div class="services_container container grid">
-            <?php if (have_rows('services')): ?>
-            <?php while (have_rows('services')): the_row(); ?>
-            <div class="services_content">
-                <div>
-                    <i class="<?php the_sub_field('service_icon'); ?> services_icon"></i>
-                    <h3 class="services_title"><?php the_sub_field('service_title'); ?></h3>
+        <!--========== Services  ==========-->
+        <section class="services section" id="services">
+            <h2 data-heading="Services" class="section_title">What I Offer</h2>
+            <div class="services_container container grid">
+                <div class="services_content">
+                    <div>
+                        <i class="uil uil-web-grid services_icon"></i>
+                        <h3 class="services_title">Web <br>Designer</h3>
+                    </div>
+                    <span class="services_button">
+                        View More <i class="uil uil-arrow-right services_button-icon"></i>
+                    </span>
+                    <div class="services_modal">
+                        <div class="services_modal-content">
+                            <i class="uil uil-times services_modal-close"></i>
+                            <h3 class="services_modal-title">Web Designer</h3>
+                            <p class="services_modal-description">Services with more than 3 years of experience. Providing quality work to clients and companies. </p>
+                            <ul class="services_modal-services grid">
+                                <li class="services_modal-service">
+                                    <i class="uil uil-check-circle services_modal-icon"></i>
+                                    <p class="services_modal-info">I develop the user interface.</p>
+                                </li>
+                                <li class="services_modal-service">
+                                    <i class="uil uil-check-circle services_modal-icon"></i>
+                                    <p class="services_modal-info">Web page development</p>
+                                </li>
+                                <li class="services_modal-service">
+                                    <i class="uil uil-check-circle services_modal-icon"></i>
+                                    <p class="services_modal-info">I create ux element</p>
+                                </li>
+                                <li class="services_modal-service">
+                                    <i class="uil uil-check-circle services_modal-icon"></i>
+                                    <p class="services_modal-info">I position your company branding </p>
+                                </li>
+                                <li class="services_modal-service">
+                                    <i class="uil uil-check-circle services_modal-icon"></i>
+                                    <p class="services_modal-info">Design and mockups of products for companies</p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
-                <span class="services_button">
-                    View More <i class="uil uil-arrow-right services_button-icon"></i>
-                </span>
-                <div class="services_modal">
-                    <div class="services_modal-content">
-                        <i class="uil uil-times services_modal-close"></i>
-                        <h3 class="services_modal-title"><?php the_sub_field('modal_title'); ?></h3>
-                        <p class="services_modal-description"><?php the_sub_field('modal_description'); ?></p>
-                        <ul class="services_modal-services grid">
-                            <?php if (have_rows('modal_services')): ?>
-                            <?php while (have_rows('modal_services')): the_row(); ?>
-                            <li class="services_modal-service">
-                                <i class="uil uil-check-circle services_modal-icon"></i>
-                                <p class="services_modal-info"><?php the_sub_field('service_info'); ?></p>
-                            </li>
-                            <?php endwhile; ?>
-                            <?php endif; ?>
-                        </ul>
+                <div class="services_content">
+                    <div>
+                        <i class="uil uil-arrow services_icon"></i>
+                        <h3 class="services_title">UI /UX <br>Designer</h3>
+                    </div>
+                    <span class="services_button">
+                        View More <i class="uil uil-arrow-right services_button-icon"></i>
+                    </span>
+                    <div class="services_modal">
+                        <div class="services_modal-content">
+                            <i class="uil uil-times services_modal-close"></i>
+                            <h3 class="services_modal-title">UI/UX Designer</h3>
+                            <p class="services_modal-description">Services with more than 3 years of experience. Providing quality work to clients and companies. </p>
+                            <ui class="services_modal-services grid">
+                                <li class="services_modal-service">
+                                    <i class="uil uil-check-circle services_modal-icon"></i>
+                                    <p class="services_modal-info">I develop the user interface.</p>
+                                </li>
+                                <li class="services_modal-service">
+                                    <i class="uil uil-check-circle services_modal-icon"></i>
+                                    <p class="services_modal-info">Web page development</p>
+                                </li>
+                                <li class="services_modal-service">
+                                    <i class="uil uil-check-circle services_modal-icon"></i>
+                                    <p class="services_modal-info">I create ux element</p>
+                                </li>
+                                <li class="services_modal-service">
+                                    <i class="uil uil-check-circle services_modal-icon"></i>
+                                    <p class="services_modal-info">I position your company branding </p>
+                                </li>
+                                <li class="services_modal-service">
+                                    <i class="uil uil-check-circle services_modal-icon"></i>
+                                    <p class="services_modal-info">Design and mockups of products for companies</p>
+                                </li>
+                            </ui>
+                        </div>
+                    </div>
+                </div>
+                <div class="services_content">
+                    <div>
+                        <i class="uil uil-edit services_icon"></i>
+                        <h3 class="services_title">Branding <br>Designer</h3>
+                    </div>
+                    <span class="services_button">
+                        View More <i class="uil uil-arrow-right services_button-icon"></i>
+                    </span>
+                    <div class="services_modal">
+                        <div class="services_modal-content">
+                            <i class="uil uil-times services_modal-close"></i>
+                            <h3 class="services_modal-title">Braning Designer</h3>
+                            <p class="services_modal-description">Services with more than 3 years of experience. Providing quality work to clients and companies. </p>
+                            <ui class="services_modal-servies grid">
+                                <li class="services_modal-service">
+                                    <i class="uil uil-check-circle services_modal-icon"></i>
+                                    <p class="services_modal-info">I develop the user interface.</p>
+                                </li>
+                                <li class="services_modal-service">
+                                    <i class="uil uil-check-circle services_modal-icon"></i>
+                                    <p class="services_modal-info">Web page development</p>
+                                </li>
+                                <li class="services_modal-service">
+                                    <i class="uil uil-check-circle services_modal-icon"></i>
+                                    <p class="services_modal-info">I create ux element</p>
+                                </li>
+                                <li class="services_modal-service">
+                                    <i class="uil uil-check-circle services_modal-icon"></i>
+                                    <p class="services_modal-info">I position your company branding </p>
+                                </li>
+                                <li class="services_modal-service">
+                                    <i class="uil uil-check-circle services_modal-icon"></i>
+                                    <p class="services_modal-info">Design and mockups of products for companies</p>
+                                </li>
+                            </ui>
+                        </div>
                     </div>
                 </div>
             </div>
-            <?php endwhile; ?>
-            <?php endif; ?>
-        </div>
-    </section>
+        </section>
 </main>
 
 <?php
